aa361a14d5fdd54060f12e76d6bde3500b0b0734
Harshad Devale <harshad.devale@tudip.nl>
2017-03-07 16:00:29 +0530

    - Added new js file for home page.

---
+++ revolv/static/js/base/homeAnimations.js
@@ -0,0 +1,146 @@
+
+$(document).ready(function() {
+
+    //style the form elements
+  if($(".form-container").length>0){
+    $('.form-container').jqTransform({imgPath:'/static/images/'});
+  }
+
+  //swipe on carousel
+  $(".carousel-inner").hammer().on('swipeleft', function(){
+    $(this).parent().find(".right.carousel-control").click();
+  });
+
+  $(".carousel-inner").hammer().on('swiperight', function(){
+    $(this).parent().find(".left.carousel-control").click();
+  });
+
+  $(".active-projects-module").css("opacity", 0);
+  $(".our-impacts-module").css("opacity", 0);
+  $(".how-it-works-module").css("opacity", 0);
+  $(".testimonial-module").css("opacity", 0);
+  $(".featured-on-module").css("opacity", 0);
+  $(".newsletter-module .container").css("opacity", 0);
+
+  //show reveal animation effect when Scrolling down the Home page
+  $(".active-projects-module").waypoint(function() {
+      $(".active-projects-module").addClass("fadeInUp");
+  }, { offset: "70%"});
+
+  if($(".our-impacts-module").length>0)
+  {
+    var Counting = false;
+    var value1 = parseInt($(".our-impacts-module .module-box .titles").eq(0).html().replace('.',''));
+    var value2 = parseInt($(".our-impacts-module .module-box .titles").eq(1).html().replace('.',''));
+    var value3 = parseInt($(".our-impacts-module .module-box .titles").eq(2).html().replace('<span class="font20">lbs</span>','').replace('.','').replace('.',''));
+    var value4 = parseInt($(".our-impacts-module .module-box .titles").eq(3).html().replace('.',''));
+
+    var step1 = parseInt(value1/(5*10));
+    step1 = (step1<1)?1:step1;
+
+    var step2 = parseInt(value2/(5*10));
+    step2 = (step2<1)?1:step2;
+
+    var step3 = parseInt(value3/(5*10));
+    step3 = (step3<1)?1:step3;
+
+    var step4 = parseInt(value4/(5*10));
+    step4 = (step4<1)?1:step4;
+
+    var currentValue1 = 0;
+    var currentValue2 = 0;
+    var currentValue3 = 0;
+    var currentValue4 = 0;
+
+    $(".our-impacts-module .module-box .titles").eq(0).html(0);
+    $(".our-impacts-module .module-box .titles").eq(1).html(0);
+    $(".our-impacts-module .module-box .titles").eq(2).html(0+'<span class="font20">lbs</span>');
+    $(".our-impacts-module .module-box .titles").eq(3).html(0);
+  }
+
+  $(".our-impacts-module").waypoint(function() {
+    $(".our-impacts-module").addClass("fadeInUp");
+
+    if(!Counting)
+    {
+      Counting = true;
+      increaseCount1(currentValue1,step1,value1);
+      increaseCount2(currentValue2,step2,value2);
+      increaseCount3(currentValue3,step3,value3);
+      increaseCount4(currentValue4,step4,value4);
+    }
+  }, { offset: "70%"});
+  $(".how-it-works-module").waypoint(function() {
+      $(".how-it-works-module").addClass("fadeInUp");
+  }, { offset: "70%"});
+  $(".testimonial-module").waypoint(function() {
+      $(".testimonial-module").addClass("fadeInUp");
+  }, { offset: "70%"});
+  $(".featured-on-module").waypoint(function() {
+      $(".featured-on-module").addClass("fadeInUp");
+  }, { offset: "70%"});
+  $(".newsletter-module").waypoint(function() {
+      $(".newsletter-module .container").addClass("fadeInUp");
+  }, { offset: "70%"});
+
+  //set parallax images for NewLetter section in Home page
+
+  if(($(window).width()+17)>= 1200 && js_urls['newsletter-bg.jpg'])
+  {
+    $('#parallax-anchor').parallax({imageSrc: js_urls['newsletter-bg.jpg']});
+  }
+  else if(js_urls['small-newsletter-bg.jpg'])
+  {
+    $('#parallax-anchor').parallax({imageSrc: js_urls['small-newsletter-bg.jpg']});
+  }
+
+  //show animation for progress bar
+  $(".status-indicator input").each(function(){$(this).attr("data-oldvalue", $(this).val());});
+  $(".status-indicator input").each(function(){$(this).val(0).trigger('change').delay(2000);});
+  $(".status-indicator input").knob({
+    'draw' : function () {
+      if (typeof this._max === 'undefined') {
+        this._max = parseInt($(this.i).data('oldvalue'));
+      }
+       $(this.i).val(this.cv + '%');
+       if(this.cv === 85 && (this._max === 100 || this._max === 0)) {
+          // Three digits, not one or two, so we make it a bit smaller
+         $(this.i).css({"font-family" : "Source Sans Pro", "font-size" : "22px", "color" : "#003399", "font-weight" : "700"});
+         $(this.i).addClass('smaller');
+       }
+       else {
+         $(this.i).css({"font-family" : "Source Sans Pro", "font-size" : "30px", "color" : "#003399", "font-weight" : "700"});
+       }
+       $(this.i).on("focus", function(){
+        $(this.i).parent().click();
+       });
+    }
+  });
+  $(".status-indicator input").each(function(){
+    animateKnob($(this));
+  });
+
+  //show pecentage value animate
+  function animateKnob ($elem) {
+    var endval = parseInt($elem.attr("data-oldvalue"));
+    var m1 = 0;
+    var tmr1;
+
+    function delayProgress(){
+      m1 += 1;
+      $elem.val(m1).trigger('change');
+      if(m1 === endval || m1 === 100) {
+        clearInterval(tmr1);
+      }
+    }
+
+    if (m1 < endval) {
+      tmr1 = self.setInterval(delayProgress,10);
+    }
+  }
+
+
+});
+
+
+
