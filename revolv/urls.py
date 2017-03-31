6f6070151968c873247ce92e67317f681407d1ee
Harshad Devale <harshad.devale@tudip.nl>
2017-04-01 01:09:37 +0530

    Revert: Rework #2483 Update number of donors.
    Details:
    - Revert the code of creating user accounts.

--- revolv/urls.py
+++ revolv/urls.py
@@ -44,7 +44,6 @@ urlpatterns = patterns(
     url(r'^my_social_account/$', 'revolv.base.views.social_connection', name='social-connection'),
     url(r'^social_connect_failed/$', 'revolv.base.views.social_exception', name='social-exception'),
 
-    url(r'^create_user_records/$', 'revolv.base.views.create_user_records', name='create_user_records'),
     url(r'^password_reset/$', base_views.password_reset_initial, name="password_reset"),
     url(r'^password_reset/done/$', base_views.password_reset_done, name="password_reset_done"),
     url(r'^password_reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$$', base_views.password_reset_confirm, name="password_reset_confirm"),
