e5877ecdbe984c1f1f8890e35c4e4b13642b3bef
harshaddevale-tudip <harshad.devale@tudip.nl>
2018-03-30 19:20:45 +0530

    Feature #5865: Re-investment - Updated the if condition on monthly_reminder file to check for 'subscribed_to_repayment_notifications' flag. - Removed the check for repayment notification status of user inside 'user_reinvestment_reminder' function.

--- revolv/tasks/monthly_reminders.py
+++ revolv/tasks/monthly_reminders.py
@@ -14,7 +14,7 @@ def user_reinvestment_reminder():
     This should execute after month_allocation
 
     This how the script do:
-    1. Read user profile with reinvest_pool >0 and subscribed_to_updates = True
+    1. Read user profile with reinvest_pool >0 and subscribed_to_repayment_notifications = True
     2. For each user send updated email
 
     """
@@ -22,21 +22,18 @@ def user_reinvestment_reminder():
     project_reinvest_list_url = SITE_URL + reverse('reinvest_list')
     project_portfolio_url = SITE_URL + reverse('dashboard')
     unsubscribe_update_url = SITE_URL + reverse('unsubscribe', kwargs={'action': 'updates'})
-    for user in RevolvUserProfile.objects.filter(reinvest_pool__gt=0.0, subscribed_to_updates=True):
+    for user in RevolvUserProfile.objects.filter(reinvest_pool__gt=0.0, subscribed_to_repayment_notifications=True):
         data = dict()
-        amount=user.reinvest_pool
-        if amount > 0.00:
-            data['amount'] = amount
-            data['portfolio_link'] = project_portfolio_url + utils.get_query_string(user.user)
-            data['projects_url'] = project_reinvest_list_url + utils.get_query_string(user.user)
-            data['unsubscribe_url'] = unsubscribe_update_url + utils.get_query_string(user.user)
-            if user.user.first_name:
-                data['first_name'] = user.user.first_name.title()
-            else:
-                data['first_name'] = 'RE-volv Supporter'
-            if user.repayment_notification:
-                send_revolv_email(
-                    'reinvestment_reminder',
-                    data, [user.user.email]
-                )
-            
+        data['amount'] = user.reinvest_pool
+        data['portfolio_link'] = project_portfolio_url + utils.get_query_string(user.user)
+        data['projects_url'] = project_reinvest_list_url + utils.get_query_string(user.user)
+        data['unsubscribe_url'] = unsubscribe_update_url + utils.get_query_string(user.user)
+        if user.user.first_name:
+            data['first_name'] = user.user.first_name.title()
+        else:
+            data['first_name'] = 'RE-volv Supporter'
+
+        send_revolv_email(
+            'reinvestment_reminder',
+            data, [user.user.email]
+        )
