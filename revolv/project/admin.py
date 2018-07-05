d9f9f974116b86a935331f7ec64be8ed67e754d0
harshaddevale-tudip <harshad.devale@tudip.nl>
2018-07-05 15:30:48 +0530

    Feature #5710: - Implemented code to add a functionality to assign multiple ambassadors to a project in the Django admin panel.

--- revolv/project/admin.py
+++ revolv/project/admin.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 
 from .models import Category, DonationLevel, Project, ProjectUpdate, AnonymousUserDetail, StripeDetails
 
+class ProjectAdmin(admin.ModelAdmin):
+    filter_horizontal = ('ambassadors',)
+
 admin.site.register(Category)
 admin.site.register(DonationLevel)
-admin.site.register(Project)
+admin.site.register(Project, ProjectAdmin)
 admin.site.register(ProjectUpdate)
 admin.site.register(AnonymousUserDetail)
 admin.site.register(StripeDetails)
