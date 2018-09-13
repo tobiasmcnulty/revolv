84266417d2905bc3da46e73c4d5a4ae00bf7819c
harshaddevale-tudip <harshad.devale@tudip.nl>
2018-09-13 11:53:12 +0530

    Feature #8033: Setting the video thumbnail as the card image for social media sharing of the project link. - Implemented a method to fetch the project video URL from the database. - Implemented a logic in the backend to parse the URL and extract the video ID. - Implemented a method to fetch the default thumnail image of the video from youtube.

--- revolv/base/templatetags/mediaurl.py
+++ revolv/base/templatetags/mediaurl.py
@@ -8,6 +8,21 @@ def fullmediaurl(value):
     """
     TODO: determine if we really need this
     """
+
+    # Checks for the youtube video ID in the video URL if found any and fetches the default thumbnail of the video.
+    query = urlparse.urlparse(value)
+    if query.hostname == 'youtu.be':
+        return 'https://img.youtube.com/vi/'+query.path[1:]+'/0.jpg'
+
+    if query.hostname in ('www.youtube.com', 'youtube.com'):
+        if query.path == '/watch':
+            p = parse_qs(query.query)
+            return 'https://img.youtube.com/vi/'+p['v'][0]+'/0.jpg'
+        if query.path[:7] == '/embed/':
+            return 'https://img.youtube.com/vi/'+query.path.split('/')[2]+'/0.jpg'
+        if query.path[:3] == '/v/':
+            return 'https://img.youtube.com/vi/'+query.path.split('/')[2]+'/0.jpg'
+
     if not value.startswith('http'):
         return settings.SITE_URL + value
 
