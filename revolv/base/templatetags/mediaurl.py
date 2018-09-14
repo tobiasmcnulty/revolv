31ada46fcec1e16e8146f53fbd935a57a290155d
harshaddevale-tudip <harshad.devale@tudip.nl>
2018-09-14 11:44:44 +0530

    Feature #8033: Setting the video thumbnail as the card image for social media sharing of the project link. - Imported the urlparse and parse_qs into the media URL file. - Implemented a method to fetch the project video URL from the database. - Implemented a logic in the backend to parse the URL and extract the video ID. - Implemented a method to fetch the default thumnail image of the video from youtube. - Updated the position of donation description panel in the portfolio page.

--- revolv/base/templatetags/mediaurl.py
+++ revolv/base/templatetags/mediaurl.py
@@ -1,3 +1,5 @@
+import urlparse
+from urlparse import parse_qs
 from django import template
 from django.conf import settings
 register = template.Library()
@@ -8,6 +10,19 @@ def fullmediaurl(value):
     """
     TODO: determine if we really need this
     """
+    # Checks for the youtube video ID in the video URL if found any and fetches the default thumbnail of the video.
+    query = urlparse.urlparse(value)
+    if query.hostname == 'youtu.be':
+        return 'https://img.youtube.com/vi/' + query.path[1:] + '/0.jpg'
+
+    if query.hostname in ('www.youtube.com', 'youtube.com'):
+        if query.path == '/watch':
+            p = parse_qs(query.query)
+            return 'https://img.youtube.com/vi/' + p['v'][0] + '/0.jpg'
+        if query.path[:7] == '/embed/':
+            return 'https://img.youtube.com/vi/' + query.path.split('/')[2] + '/0.jpg'
+        if query.path[:3] == '/v/':
+            return 'https://img.youtube.com/vi/' + query.path.split('/')[2] + '/0.jpg'
     if not value.startswith('http'):
         return settings.SITE_URL + value
 
