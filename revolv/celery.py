247351853846808380f76e61decb08bf2935a820
revolv-dev <mark@re-volv.org>
2020-03-27 12:26:18 -0700

    maintenace - comment out celery code

--- revolv/celery.py
+++ revolv/celery.py
@@ -16,10 +16,10 @@ from django.conf import settings  # noqa
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
-app.config_from_object('django.conf:settings')
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+#app.config_from_object('django.conf:settings')
+#app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 
-@app.task(bind=True)
-def debug_task(self):
-    print('Request: {0!r}'.format(self.request))
+#@app.task(bind=True)
+#def debug_task(self):
+#    print('Request: {0!r}'.format(self.request))
